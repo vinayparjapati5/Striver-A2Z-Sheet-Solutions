5929dcdc6bb2bcd50320a060d913f0b695825e87
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-04-02 21:02:49 +0530

    Create Reverse Linked list.cpp

---
+++ Linked List/Reverse Linked list.cpp
@@ -0,0 +1,44 @@
+ListNode* reverseList(ListNode* head) {
+        //brute ->using stack
+        ListNode*temp = head;
+        stack<int>st;
+
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp=temp->next;
+        }
+        temp= head;
+
+        while(temp){
+
+            temp->val = st.top();
+            st.pop();
+            temp=temp->next;
+        }
+        return head; 
+
+        //iterative 
+
+        ListNode*temp = head, *prev = nullptr;
+
+        while(temp){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev; 
+
+        //Recursive Solution
+
+        if(head==NULL || head->next == NULL){
+            return head;
+        }
+        ListNode*Newhead = reverseList(head->next);
+
+        ListNode*front = head->next;
+        front->next = head;
+        head->next = nullptr;
+
+        return Newhead;
+    }
