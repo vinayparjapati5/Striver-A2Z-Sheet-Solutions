2e1cd5158879c6075aef6827857a75c9bc744ec5
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-04-05 23:25:47 +0530

    Create Palindrome Linked list.cpp

---
+++ Linked List/Palindrome Linked list.cpp
@@ -0,0 +1,25 @@
+bool isPalindrome(ListNode* head) {
+
+  // BRUTE FORCE using stack
+
+ /* stack<int>st;
+  ListNode*temp = head;
+  while(temp!=NULL){
+    st.push(temp->val);
+    temp = temp->next;
+  }
+
+  temp= head;
+  while(temp!=NULL){
+    if(temp->val!=st.top()){
+      return false;
+    }
+    temp = temp->next;
+    st.pop();
+  }
+  return true ; */
+
+
+  
+
+}
