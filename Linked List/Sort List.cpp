07da787e6a56fab15e29a31636fdcc64ca268c2d
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-04-16 23:02:12 +0530

    Update Sort List.cpp

--- Linked List/Sort List.cpp
+++ Linked List/Sort List.cpp
@@ -25,5 +25,25 @@ ListNode* sortList(ListNode* head) {
 
 
   // OPTIMAL ONE ->MERGE SORT 
+1. first find middle (first middle) of Linked list
+2. then apply merge sort
+3. then merge 2 sort lists
+  
+        if(head==NULL || head->next ==NULL){
+            return head;
+        }
+
+        ListNode*middle = findMiddle(head);
+        ListNode*right = middle->next;
+        
+        middle->next = nullptr;
+
+        ListNode*left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return Merge2(left,right);
+        
         
 }
