1f5b454b75c1f60d479bb5f1b9ff78e714ab45a7
Vinay Kumar <114856104+vinayparjapati5@users.noreply.github.com>
2024-04-29 11:46:29 +0530

    Update Rotate List.cpp

--- Linked List/Rotate List.cpp
+++ Linked List/Rotate List.cpp
@@ -1,1 +1,37 @@
 
+    ListNode* findNthNode(ListNode*temp, int k){
+        int cnt = 1;
+
+        while(temp!=NULL){
+            if(cnt ==k){
+                return temp;
+            }
+            cnt++;
+            temp = temp->next;
+        }
+        return temp;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head==NULL || head->next == NULL){
+            return head;
+        }
+       int len = 1;
+       ListNode*tail = head;
+
+       while(tail->next!=NULL){
+        len++;
+        tail = tail->next;
+       }
+       if(k%len==0){
+        return head;
+       }
+
+       k = k%len;
+       tail->next = head;
+
+       ListNode*newLastNode = findNthNode(head,len-k);
+       head = newLastNode->next;
+       newLastNode->next = NULL;
+
+       return head;
+    }
